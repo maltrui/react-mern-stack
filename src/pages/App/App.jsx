ed76ac9741d39583f4d653f08c71c8d3c4958a35
Jim Clark <jim.clark@generalassemb.ly>
2022-08-26 10:21:42 -0700

    Finish Part 2

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import AuthPage from '../AuthPage/AuthPage';
 import NewOrderPage from '../NewOrderPage/NewOrderPage';
 import OrderHistoryPage from '../OrderHistoryPage/OrderHistoryPage';
+import NavBar from '../../components/NavBar/NavBar';
 import './App.css';
 
 export default function App() {
@@ -11,11 +12,14 @@ export default function App() {
   return (
     <main className="App">
       { user ?
-        <Routes>
-          {/* Route components in here */}
-          <Route path='/orders/new' element={<NewOrderPage />} />
-          <Route path='/orders' element={<OrderHistoryPage />} />
-        </Routes>
+        <>
+          <NavBar />
+          <Routes>
+            {/* Route components in here */}
+            <Route path='/orders/new' element={<NewOrderPage />} />
+            <Route path='/orders' element={<OrderHistoryPage />} />
+          </Routes>
+        </>
         :
         <AuthPage />
       }
