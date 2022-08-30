d721164f4a40bbe1b8b91906b258d72c9b1339b8
Jim Clark <jim.clark@generalassemb.ly>
2022-08-30 15:50:12 -0700

    Finish checkToken

--- src/utilities/users-api.js
+++ src/utilities/users-api.js
@@ -1,37 +1,17 @@
 // API modules are used to perform the AJAX
 // communications between client (browser)
 // and the server
-
+import sendRequest from './send-request';
 const BASE_URL = '/api/users';
 
-export async function signUp(userData) {
-  // Fetch uses an options object as a second arg
-  // to make requests other than basic GET requests,
-  // include data, set headers, etc
-  const res = await fetch(BASE_URL, {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify(userData)
-  });
-  if (res.ok) {
-    return res.json();
-  } else {
-    throw new Error('Invalid Sign Up');
-  }
+export function signUp(userData) {
+  return sendRequest(BASE_URL, 'POST', userData);
 }
 
-export async function login(credentials) {
-  // Fetch uses an options object as a second arg
-  // to make requests other than basic GET requests,
-  // include data, set headers, etc
-  const res = await fetch(`${BASE_URL}/login`, {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify(credentials)
-  });
-  if (res.ok) {
-    return res.json();
-  } else {
-    throw new Error('Invalid Log In');
-  }
+export function login(credentials) {
+  return sendRequest(`${BASE_URL}/login`, 'POST', credentials);
+}
+
+export function checkToken() {
+  return sendRequest(`${BASE_URL}/check-token`);
 }
