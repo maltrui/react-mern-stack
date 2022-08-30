0eb4f08974d58671835b84852f6c46d63729401f
Jim Clark <jim.clark@generalassemb.ly>
2022-08-30 09:42:43 -0700

    Add token utility functions

--- src/utilities/users-service.js
+++ src/utilities/users-service.js
@@ -12,6 +12,27 @@ export async function signUp(userData) {
   // Delegate the request to the users-api
   // which will ultimatelly return a JSON Web Token (JWT)
   const token = await usersAPI.signUp(userData);
+  // Persist the token 
+  localStorage.setItem('token', token);
   // baby step (TODO: return a user object)
   return token;
+}
+
+// Return the token if valid, otherwise return null
+export function getToken() {
+  const token = localStorage.getItem('token');
+  if (!token) return null;
+  // Obtain the payload of the token
+  // so that we can check if it's expired
+  const payload = JSON.parse(atob(token.split('.')[1]));
+  if (payload.exp < Date.now() / 1000) {
+    localStorage.removeItem('token');
+    return null;
+  }
+  return token;
+}
+
+export function getUser() {
+  const token = getToken();
+  return token ? JSON.parse(atob(token.split('.')[1])).user : null;
 }
